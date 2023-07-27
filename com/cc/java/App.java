f7f83b907fa5c4e351812e1c202d0684aa4d4aa8
Anton Haustov <anton.haust@gmail.com>
2023-07-27 15:47:07 +0200

    solution codelab challenge

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,14 +1,32 @@
 package com.cc.java;
 
-public class App {
-    
-    public static void main(String[] args) {
-    
-    }
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class App {
+
+    public static void main(String[] args) {
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+        Konto konto3 = new Konto();
+
+        List<Konto> konten = new ArrayList<Konto>(Arrays.asList(konto1, konto2, konto3));
+
+        outputAll(konten);
+        konten.get(0).setKontostand(konten.get(0).getKontostand() * 2);
+        konten.get(1).setKontostand(konten.get(1).getKontostand() * 3);
+        konten.get(2).setKontostand(konten.get(2).getKontostand() * 10);
+        outputAll(konten);
+    }
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
-    } 
+    }
 
+    private static void outputAll(List<Konto> konten) {
+        for (Konto konto : konten) {
+            output(String.valueOf(konto.getKontostand()));
+        }
+    }
 }
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,21 @@
+package com.cc.java;
+
+public class Konto {
+    private int kontostand = 10000;
+
+    public Konto() {
+    }
+
+    public Konto(int kontostand) {
+        this.kontostand = kontostand;
+    }
+
+    public int getKontostand() {
+        return this.kontostand;
+    }
+
+    public void setKontostand(int i) {
+        this.kontostand = i;
+    }
+
+}
